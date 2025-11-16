96c10897d5383667c0361aec779b72c22e31ab13
Rodrigo <ro.anasco.s@gmail.com>
2025-11-15 23:39:44 -0800

    created the model, opened video fine

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv4/opencv2/videoio.hpp>
+
 
 int main() {
-    std::cout << "Hello World" << std::endl;
     
+    // Model gets imported in ONNX form and its stored into a "net" (that stands for Neural Network)
+    cv::dnn::Net net = cv::dnn::readNetFromONNX("../src/yolov8n.onnx"); //import works fine!
+    
+    //video reader - working fine
+    cv::VideoCapture video("../videos/walking_people.mp4");
+    if (video.isOpened() == false){
+        std::cout << "Cannot Open Video" << std::endl;
+    }
+
+    
+    std::cout << "Program Working Fine" << std::endl;
+
     return 0;
 }
